dfa8ffebf4ee923de5f4e142e807958ea404083d
Yash_borekar <yash.fkkfr22@sinhgad.edu>
2025-02-05 16:24:50 +0530

    exploring dnamic values in jsx

--- React-Yash/src/App.jsx
+++ React-Yash/src/App.jsx
@@ -14,17 +14,24 @@ export const App = () => {
 };
 
 const NetFlixCard = () => {
+  const name = "Kung Fu Panda";
+  // const rating = ;
+  const Summary = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt praesentium aspernatur minima, ullam illum, error voluptatibus quidem veritatis, impedit amet ipsa hic totam perspiciatis voluptatum culpa ex maxime facere aliquam possimus non deserunt neque quas!";
+  const returnGenre = () =>{
+    const genre = "Animation Comedy";
+    return genre;
+  };
+  
   return (
-    <div>
-    
-
-    <div>
+    <div>  
+    <div >
       <img src="panda.jpeg" alt="panda.jpeg"/>
     </div>
     <div>
-     <h2>Kung Fu Panda</h2>
-     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt praesentium aspernatur minima, ullam illum, error voluptatibus quidem veritatis, impedit amet ipsa hic totam perspiciatis voluptatum culpa ex maxime facere aliquam possimus non deserunt neque quas!</p>
-     <h1>Ratings : 9.0 / 10</h1>
+     <h2>Name : {name}</h2>
+     <p>Summary : {Summary}</p>
+     <h2>Ratings :{5+4.1} </h2>
+     <h3>Genre : {returnGenre()}</h3>
     </div>
  
   </div>
