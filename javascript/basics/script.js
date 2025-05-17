25a737a1a973f846ef3dbd11d58ec7c7c6701b96
Yash_borekar <yash.fkkfr22@sinhgad.edu>
2025-05-17 21:55:26 +0530

    inbuilt feature

---
+++ javascript/basics/script.js
@@ -0,0 +1,9 @@
+var a= 10;
+var b = 15;
+
+
+// console.log(a+b); can be checked on console in browser
+// alert("Addition-"+(a+b)) shows popup box
+// var age = prompt("Enter your age") can take input
+// console.log("age - "+age)
+
