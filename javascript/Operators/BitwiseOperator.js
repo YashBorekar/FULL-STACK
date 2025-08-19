bc18d1bdf86b5226d9530838e1ef0706de9efab9
YashBorekar <yashborekar1@gmail.com>
2025-08-19 23:34:54 +0530

    Add Bitwise and Logical operator examples

---
+++ javascript/Operators/BitwiseOperator.js
@@ -0,0 +1,19 @@
+// Bitwise AND
+let x = 5;  // 0101
+let y = 3;  // 0011
+console.log(x & y); // 1 (0001)
+
+// Bitwise OR
+console.log(x | y); // 7 (0111)
+
+// Bitwise NOT
+console.log(~x);    // -6 (inverts all bits)
+
+// Bitwise XOR
+console.log(x ^ y); // 6 (0110) 
+
+// right shift
+console.log(x >> 1); // 2 (0010)
+
+// left shift
+console.log(x << 1); // 10 (1010)
---
+++ javascript/Operators/LogicalOperator.js
@@ -0,0 +1,11 @@
+// Logical AND 
+let a = 18;
+let b = 19;
+console.log(a < b && b > 15); // true
+
+// Logical OR
+console.log(a > b || b > 15); // true
+
+// Logical NOT
+console.log(!(a < b)); // false
+
